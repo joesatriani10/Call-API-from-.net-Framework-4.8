039898a238d68f9a801e48f22f07ca2ee3760674
Angel Montes de Oca <joesatriani10@gmail.com>
2023-10-23 22:11:01 -0700

    Add project files.

---
+++ Call API from .net Framework 4.8/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Call_API_from.net_Framework_4._8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtAPI = new System.Windows.Forms.TextBox();
+            this.lblLoginAPI = new System.Windows.Forms.Label();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(145, 57);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(230, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(9, 22);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(9, 60);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(7, 9);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(65, 12);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(7, 45);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(62, 12);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtAPI
+            // 
+            this.txtAPI.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAPI.Location = new System.Drawing.Point(145, 22);
+            this.txtAPI.Name = "txtAPI";
+            this.txtAPI.Size = new System.Drawing.Size(230, 20);
+            this.txtAPI.TabIndex = 5;
+            // 
+            // lblLoginAPI
+            // 
+            this.lblLoginAPI.AutoSize = true;
+            this.lblLoginAPI.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginAPI.Location = new System.Drawing.Point(143, 9);
+            this.lblLoginAPI.Name = "lblLoginAPI";
+            this.lblLoginAPI.Size = new System.Drawing.Size(60, 12);
+            this.lblLoginAPI.TabIndex = 6;
+            this.lblLoginAPI.Text = "Login API:";
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.BackColor = System.Drawing.SystemColors.Desktop;
+            this.txtConsole.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtConsole.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsole.ForeColor = System.Drawing.Color.Green;
+            this.txtConsole.Location = new System.Drawing.Point(12, 105);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.Size = new System.Drawing.Size(776, 333);
+            this.txtConsole.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.lblLoginAPI);
+            this.Controls.Add(this.txtAPI);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Call API from .Net Framework";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtAPI;
+        private System.Windows.Forms.Label lblLoginAPI;
+        private System.Windows.Forms.TextBox txtConsole;
+    }
+}
+
---
+++ Call API from .net Framework 4.8/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Call_API_from.net_Framework_4._8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Attach the event handler to the TextChanged event of each TextBox
+            txtUsername.TextChanged += TextBoxes_TextChanged;
+            txtPassword.TextChanged += TextBoxes_TextChanged;
+            txtAPI.TextChanged += TextBoxes_TextChanged;
+
+            // Initial validation
+            ValidateLoginButton();
+        }
+
+
+        private void TextBoxes_TextChanged(object sender, EventArgs e)
+        {
+            ValidateLoginButton();
+        }
+
+        private void ValidateLoginButton()
+        {
+            // Disable btnLogin if any of the TextBoxes are empty or null
+            btnLogin.Enabled = !string.IsNullOrWhiteSpace(txtUsername.Text) &&
+                               !string.IsNullOrWhiteSpace(txtPassword.Text) &&
+                               !string.IsNullOrWhiteSpace(txtAPI.Text);
+        }
+
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private async void btnLogin_Click(object sender1, EventArgs e)
+        {
+            btnLogin.Enabled = false;
+
+            System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, cert, chain, sslPolicyErrors) =>
+                    true; //Accept All Certificates(Not Recommended for Production)
+            try
+            {
+                txtConsole.Text = @"Calling API...";
+                var data = new
+                {
+                    username = txtUsername.Text,
+                    password = txtPassword.Text
+                };
+
+
+                var json = JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(txtAPI.Text, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    txtConsole.Text = result;
+                }
+                else
+                {
+                    txtConsole.Text = $@"Error: {response.StatusCode} {await response.Content.ReadAsStringAsync()}";
+                }
+
+                btnLogin.Enabled = true;
+            }
+            catch (HttpRequestException er)
+            {
+                string errorMessage = $"Request error: {er.Message}";
+
+                if (er.InnerException != null)
+                {
+                    errorMessage += $"\nInner Exception: {er.InnerException.Message}";
+                }
+
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnLogin.Enabled = true;
+            }
+        }
+    }
+}
